d8d22d26022ad9718379c687c7d90605b4fded48
wml <1600214592@qq.com>
2023-01-30 17:17:12 +0800

    yes

---
+++ app/src/main/java/com/example/test_table/Table.java
@@ -0,0 +1,15 @@
+package com.example.test_table;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.TableLayout;
+
+public class Table extends TableLayout {
+    public Table(Context context) {
+        super(context);
+    }
+
+    public Table(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
